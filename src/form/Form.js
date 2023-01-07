33310906ef097ae7f18aafaf6ac56b6d53c99f59
ishaninfo56@gmail.com <IsHaNinfo>
2023-01-07 09:32:44 +0530

    add the profile pic

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./form.css";
 import InputBox from "./InputBox";
+import Avatar from "./Avatar";
 
 const Form = () => {
   return (
@@ -8,6 +9,10 @@ const Form = () => {
       <div className="heading">
         <h1>Create your Account</h1>
       </div>
+      <Avatar
+        src="https://img.freepik.com/premium-vector/man-avatar-profile-round-icon_24640-14044.jpg?w=740"
+        alt="Avatar"
+      />
       <InputBox />
     </div>
   );
